1ac54c7b897273bf669a2fec8fa347920fd39c49
Markus Hölzle <maxwiese13@gmail.com>
2019-08-22 10:40:04 +0200

    add my first model and tested it (refs #7)

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const odata = require('node-odata');
 const config = require('config');
 
+// utils
 const {encryptPasswort, validatePasswort} = require('./utils/encryption');
- 
+
+//models
+const userModel = require('./models/user');
+
 //connect server to database
 const db_path = config.get('odata.db_path');
 var server = odata(db_path);
 
 //TODO: specifi models (#7)
-server.resource();
+server.resource('users', userModel);
 
 // start the server on port 3000
 server.listen(3000);
---
+++ models/user.js
@@ -0,0 +1,4 @@
+module.exports = {
+    email: String,
+    password: String,
+}
